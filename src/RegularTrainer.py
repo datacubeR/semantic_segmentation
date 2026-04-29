I added image logging. TODO: lr and bs tuner, better validation images for tensorboard... Fixed potential memory issue accumulating gradients...

src/RegularTrainer.py
@@ -18,6 +18,9 @@ from .datamodules import data_classes as DM
 from .segmentators import RegularTrainingSegmentator
 
 if __name__ == "__main__":
+    # ============================================================
+    # Argument Parsing and Config Loading
+    # ============================================================
     argparse = argparse.ArgumentParser(description="Model Trainer")
     argparse.add_argument(
         "--config",
@@ -36,6 +39,10 @@ if __name__ == "__main__":
         exit(1)
 
     try:
+        # ============================================================
+        # Project Setup
+        # ============================================================
+        L.seed_everything(config.get("seed", 42), workers=True)
         VERSION = config["version"]
         PROJECT_NAME = config["project_name"]
         MODEL_NAME = config["model_name"]
@@ -51,6 +58,9 @@ if __name__ == "__main__":
         val_deadtrees_image_glob = config["data"]["val_image_glob"]
         val_deadtrees_mask_glob = config["data"]["val_mask_glob"]
 
+        # ============================================================
+        # DataModule
+        # ============================================================
         deadtrees_dm = DM.RegularDataModule(
             dataset_cls=getattr(DM, config["data"]["dataset_cls"]),
             train_dataset_kwargs={
@@ -69,6 +79,9 @@ if __name__ == "__main__":
             },
         )
 
+        # ============================================================
+        # Model Definition
+        # ============================================================
         model = Unet(
             config["model"]["encoder_name"],
             encoder_weights=config["model"]["encoder_weights"],
@@ -76,6 +89,9 @@ if __name__ == "__main__":
             classes=config["model"]["classes"],
         )
 
+        # ============================================================
+        # Augmentation Setup
+        # ============================================================
         if config["augmentation"]["apply"]:
             horizontal_flip = config["augmentation"]["horizontal_flip"]
             vertical_flip = config["augmentation"]["vertical_flip"]
@@ -110,6 +126,9 @@ if __name__ == "__main__":
             else None
         )
 
+        # ============================================================
+        # Lightning Module Setup
+        # ============================================================
         segmentator = RegularTrainingSegmentator(
             model=model,
             criterion=getattr(smpl, config["architecture"]["criterion"])(
@@ -119,17 +138,25 @@ if __name__ == "__main__":
             optimizer_cls=getattr(torch.optim, config["architecture"]["optimizer"]),
             optimizer_kwargs=config["architecture"]["optimizer_kwargs"],
             metric=DiceScore(**config["architecture"]["metrics_kwargs"]),
+            n_tb_images=config["trainer"]["n_tb_images"],
+            tb_size=tuple(config["trainer"]["tb_size"]),
             add_channel_dim=config["architecture"]["add_channel_dim"],
         )
 
+        # ============================================================
+        # Model Checkpointing
+        # ============================================================
         checkpoint_callback = ModelCheckpoint(
             dirpath=f"checkpoints/{PROJECT_NAME}/version_{VERSION}/checkpoints",
-            filename="{epoch:02d}-{step}-{val_dice:.4f}-{val_loss:.4f}",
+            filename="{epoch:02d}-{step}-{val_metric:.4f}-{val_loss:.4f}",
             save_top_k=1,  # keep best 3 models
             monitor="val_metric",  # or "val_loss"
             mode="max",  # IoU → maximize
             save_last=True,  # VERY IMPORTANT for resume
         )
+        # ============================================================
+        # Logger and Trainer Setup
+        # ============================================================
         logger = TensorBoardLogger(
             save_dir="logs", version=VERSION, name=f"{PROJECT_NAME}"
         )
@@ -149,6 +176,9 @@ if __name__ == "__main__":
             accumulate_grad_batches=config["trainer"]["accumulate_grad_batches"],
         )
 
+        # ============================================================
+        # Script Setup and Training Execution
+        # ============================================================
         if config["fast_dev_run"]:
             print(
                 "[bold red] Fast development run enabled. Training run for 1 batch only for Debugging Purposes only. [/bold red]"

src/segmentators/regular_segmentator.py
@@ -1,5 +1,8 @@
+import kornia.geometry as kg
 import lightning as L
+import torch
 import torch.nn.functional as F
+from torchvision.utils import make_grid
 
 
 class RegularTrainingSegmentator(L.LightningModule):
@@ -11,6 +14,8 @@ class RegularTrainingSegmentator(L.LightningModule):
         metric,
         optimizer_cls,
         optimizer_kwargs,
+        n_tb_images=4,
+        tb_size=(128, 128),
         add_channel_dim=False,
     ):
         super().__init__()
@@ -21,6 +26,9 @@ class RegularTrainingSegmentator(L.LightningModule):
         self.optimizer_cls = optimizer_cls
         self.optimizer_kwargs = optimizer_kwargs
         self.add_channel_dim = add_channel_dim
+        self.n_tb_images = n_tb_images
+        self.tb_size = tb_size
+        self.lr = optimizer_kwargs["lr"]
 
     def forward(self, x):
         return self.model(x)
@@ -84,12 +92,72 @@ class RegularTrainingSegmentator(L.LightningModule):
     def on_fit_start(self):
         self.metric = self.metric.to(self.device)
 
+    def on_validation_batch_start(self, batch, batch_idx, dataloader_idx=0):
+        # Saves the first batch once for validation purposes in TensorBoard
+        if batch_idx == 0 and self.current_epoch == 0:
+            self.tensorboard_images, self.tensorboard_masks = (
+                batch["image"][: self.n_tb_images],
+                batch["mask"][: self.n_tb_images],
+            )
+
     def on_validation_epoch_end(self):
         miou = self.metric.compute()
-        self.log(
-            "val_metric", miou, on_epoch=True, prog_bar=True, batch_size=1, logger=True
-        )
+        self.log("val_metric", miou, on_epoch=True, prog_bar=True, logger=True)
         self.metric.reset()
+
+        with torch.no_grad():
+            logits = self(self.tensorboard_images.to(self.device))
+            preds = logits.argmax(dim=1)
+
+        resized_image = kg.resize(self.tensorboard_images, self.tb_size)
+        resized_mask = (
+            kg.resize(
+                self.tensorboard_masks.float(), self.tb_size, interpolation="nearest"
+            )
+            .squeeze(1)
+            .long()
+        )
+        resized_preds = (
+            kg.resize(preds.float(), self.tb_size, interpolation="nearest")
+            .squeeze(1)
+            .long()
+        )
+
+        pred_rgb = self._mask_to_rgb(resized_preds)
+        mask_rgb = self._mask_to_rgb(resized_mask)
+        grid = make_grid(
+            torch.cat([resized_image, mask_rgb, pred_rgb], dim=0),
+            nrow=4,
+            normalize=True,
+        )
+        self.logger.experiment.add_image("val/img|gt|pred", grid, self.current_epoch)
+
+    def _mask_to_rgb(self, mask):
+        colors = (
+            torch.tensor(
+                ## give me a list of several colors in rgb
+                [
+                    [0, 0, 0],  # class 0 -> black
+                    [255, 0, 0],  # class 1 -> red
+                    [0, 255, 0],  # class 2 -> green
+                    [0, 0, 255],  # class 3 -> blue
+                    [255, 255, 0],  # class 4 -> yellow
+                    [255, 0, 255],  # class 5 -> magenta
+                    [0, 255, 255],  # class 6 -> cyan
+                    [128, 128, 128],  # class 7 -> gray
+                    [68, 55, 39],  # class 8 -> brown
+                    [255, 165, 0],  # class 9 -> orange
+                    [128, 0, 128],  # class 10 -> purple
+                ],
+                dtype=torch.float32,
+                device=mask.device,
+            )
+            / 255.0
+        )
+
+        # B, H, W = mask.shape
+        rgb = colors[mask.long()]  # (B, H, W, 3)
+        return rgb.permute(0, 3, 1, 2)  # (B, 3, H, W)
 
     def configure_optimizers(self):
         optimizer = self.optimizer_cls(self.parameters(), **self.optimizer_kwargs)